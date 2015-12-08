aa6ab101aa461bf8ba175c5dd1583d4e96d8423d
vivlong <vivlonglz@gmail.com>
2015-12-08 13:39:39 +0800

    Fix Jmjm1

--- API/API.ServiceModel/Tables/Jmjm1.cs
+++ API/API.ServiceModel/Tables/Jmjm1.cs
@@ -4,26 +4,30 @@ using System.Linq;
 using System.Text;
 
 namespace WebApi.ServiceModel.Tables
-{
-    public class Jmjm1
-    {
-        public string JobNo { get; set; }
-        public string AwbBlNo { get; set; }
-        public string CustomerCode { get; set; }
-        public string JobType { get; set; }
-        public string ModuleCode { get; set; }
-        public string CustomerRefNo { get; set; }
-        public Nullable<System.DateTime> Eta { get; set; }
-        public Nullable<System.DateTime> Etd { get; set; }
-        public string OriginCode { get; set; }
-        public string DestCode { get; set; }
-        public string VesselCode { get; set; }
-        public string VesselName { get; set; }
-        public string VoyageNo { get; set; }
-        public int Pcs { get; set; }
-        public decimal GrossWeight { get; set; }
-        public decimal Volume { get; set; }
-        public string AttachmentFlag { get; set; }
-        public string StatusCode { get; set; }
-    }
+{	
+	public class Jmjm1
+	{
+		public string JobNo { get; set; }
+		public string AwbBlNo { get; set; }
+		public string MAwbOblNo { get; set; }
+		public string CustomerCode { get; set; }
+		public string JobType { get; set; }
+		public string ModuleCode { get; set; }
+		public string CustomerRefNo { get; set; }
+		public Nullable<System.DateTime> ETA { get; set; }
+		public Nullable<System.DateTime> ETD { get; set; }
+		public string CommodityDescription { get; set; }	
+		public string OriginCode { get; set; }
+		public string OriginName { get; set; }
+		public string DestCode { get; set; }
+		public string DestName { get; set; }
+		public string VesselCode { get; set; }
+		public string VesselName { get; set; }
+		public string VoyageNo { get; set; }
+		public int Pcs { get; set; }
+		public decimal GrossWeight { get; set; }
+		public decimal Volume { get; set; }
+		public string AttachmentFlag { get; set; }
+		public string StatusCode { get; set; }
+	}
 }
